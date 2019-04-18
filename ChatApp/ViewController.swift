38185a374111203837bff0898acf20ec8e91a49e
Adarsh E <adarshe@focaloid.com>
2019-04-18 10:11:45 +0530

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Focaloid on 18/04/19.
+//  Copyright © 2019 Focaloid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
